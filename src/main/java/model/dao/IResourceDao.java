17ae78853c8902c2b0dc6de1b4c768ef6c2dccd6
ilyashq <ilyasenaa24@gmail.com>
2024-05-23 18:28:07 +0100

    Création des classes de ressource dans le DAO

---
+++ src/main/java/model/dao/IResourceDao.java
@@ -0,0 +1,18 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Project;
+import model.entities.Ressource;
+
+public interface IResourceDao {
+
+	public Ressource save (Ressource r);
+	public List <Ressource> getAllressource();
+	public List <Ressource> RessourceParMc(String mc);
+	public Project update (Ressource r);
+	public void deleteRessource(int id);
+	public Ressource getRessourceById(int id);
+	
+	
+}
